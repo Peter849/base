703e1aa38837507142d6126133776c58eda6290a
Vakán Péter <peter.vakan13@gmail.com>
2023-03-27 19:50:59 +0200

    Adding a new unit test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -7,13 +7,22 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController controller;
+	TrainSensor sensor;
+	TrainUser user;
+
     @Before
     public void before() {
         // TODO Add initializations
+        TrainSystem system = new TrainSystem();
+		controller = system.getController();
+		sensor = system.getSensor();
+		user = system.getUser();
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void testOverrideSpeedLimit() {
+        sensor.overrideSpeedLimit(120);
+        Assert.assertEquals(120, sensor.getSpeedLimit());
     }
 }
