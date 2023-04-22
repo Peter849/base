9d483c7f1242a11a467ce5e2998c50f52be0e306
Vakán Péter <peter.vakan13@gmail.com>
2023-04-22 21:42:20 +0200

    periodic ref

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,36 +2,54 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+/**
+ * Primary controller implementation
+ */
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-	private boolean eBreak = false;
+	private boolean overrideSpeed = false;
+	private boolean move = false;
+	// In seconds
+	private static int timeLimit = 5;
 
 	@Override
 	public void followSpeed() {
-		if(!eBreak){
-			if (referenceSpeed < 0) {
-				referenceSpeed = 0;
-			} else {
-				if(referenceSpeed+step > 0) {
-					referenceSpeed += step;
-				} else {
-					referenceSpeed = 0;
-				}
-			}
-		}else{
-			if(referenceSpeed > 0){
-				if(step > 0)referenceSpeed -= step;
-				else referenceSpeed += step;
-				if(referenceSpeed < 0) referenceSpeed = 0;
-			}else{
-				this.eBreak = false;
-			}
+		if (referenceSpeed < 0) {
+			referenceSpeed = 0;
+		} else {
+		    if(referenceSpeed+step > 0) {
+                referenceSpeed += step;
+            } else {
+		        referenceSpeed = 0;
+            }
 		}
-		
+
 		enforceSpeedLimit();
+	}
+
+	@Override
+	public void runSimulation() throws InterruptedException {
+		while(move) {
+			followSpeed();
+			Thread.sleep(timeLimit * 1000);
+		}
+	}
+
+	@Override
+	public void startSimulation() {
+		move = true;
+	}
+
+	@Override
+	public void stopSimulation() {
+		move = false;
+	}
+
+	public static void setTimeLimit(int l) {
+		timeLimit = l;
 	}
 
 	@Override
@@ -47,9 +65,13 @@ public class TrainControllerImpl implements TrainController {
 	}
 
 	private void enforceSpeedLimit() {
-		if (referenceSpeed > speedLimit) {
+		if (!overrideSpeed && referenceSpeed > speedLimit) {
 			referenceSpeed = speedLimit;
 		}
+	}
+
+	public void setOverrideSpeed(boolean var){
+		overrideSpeed = var;
 	}
 
 	@Override
@@ -57,8 +79,4 @@ public class TrainControllerImpl implements TrainController {
 		this.step = joystickPosition;		
 	}
 
-	@Override
-	public void emergencyBreak(){
-		this.eBreak = true;
-	}
 }
--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
@@ -2,6 +2,12 @@ package hu.bme.mit.train.interfaces;
 
 public interface TrainController {
 
+	public void runSimulation() throws InterruptedException;
+
+	public void startSimulation();
+
+	public void stopSimulation();
+
 	void followSpeed();
 
 	int getReferenceSpeed();
